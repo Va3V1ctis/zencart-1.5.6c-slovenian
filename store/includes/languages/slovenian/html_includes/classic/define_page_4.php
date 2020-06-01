dfcb030e46f1842f3a157f17b1e5b5fc717a38c1
Va3V1ctis <47570062+Va3V1ctis@users.noreply.github.com>
2020-06-01 18:34:25 +0200

    Add files via upload

--- store/includes/languages/slovenian/html_includes/classic/define_page_4.php
+++ store/includes/languages/slovenian/html_includes/classic/define_page_4.php
@@ -1,4 +1,5 @@
-<p><strong>Checkout Success Sample Text ...</strong></p><p>A few words about the approximate shipping time or your processing policy would be put here. </p>
+<p><strong>Page 4 Sample Text ...</strong></p>
 <p>This section of text is from the Define Pages Editor located under Tools in the Admin.</p>
+<p>To remove this section of the text, delete it from the Define Pages Editor.</p>
 <p>This file is located in<code> /languages/english/html_includes/classic/</code></p>
 <p><strong>NOTE: Always backup the files in<code> /languages/english/html_includes/your_template</code></strong></p>
